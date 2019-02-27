f3886000bb96d2e5327c098653303fc130600e19
Slawek12Q <slawek.guzik@interia.pl>
2019-02-27 22:29:26 +0100

    new program

---
+++ Spring_MVP_FirstStep/src/main/java/spring_mvn/example/practise/SpringMvnPractiseApplication.java
@@ -0,0 +1,13 @@
+package spring_mvn.example.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvnPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvnPractiseApplication.class, args);
+    }
+
+}
---
+++ Spring_MVP_FirstStep/src/test/java/spring_mvn/example/practise/SpringMvnPractiseApplicationTests.java
@@ -0,0 +1,16 @@
+package spring_mvn.example.practise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvnPractiseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
