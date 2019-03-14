01f4c830e843aaedbabe8b763e22072a3492f111
Slawek12Q <slawek.guzik@interia.pl>
2019-03-14 22:33:54 +0100

    rest_practise

---
+++ Json-spring-boot/src/main/java/pl/slawek/model/City.java
@@ -0,0 +1,32 @@
+package pl.slawek.model;
+
+public class City {
+    private String name;
+    private int population;
+
+    City() {
+    }
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+
+}
